ce9955f1fd8a25739448d1399b1db32eb70dfd5e
Ridwan346 <ridwanhossain955@gmail.com>
2025-12-06 19:47:41 +0600

    third commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let category_nam =(categorise)=>{
     div_right.innerText =""
     for(let category of categorise ){
         let nw = document.createElement("div")
-        nw.innerHTML=`<div class="p-3"><button>${category.category_name}</button></div>`
+        nw.innerHTML=`<div class="p-3"><button onclick= "catagory_name(${category.id})">${category.category_name}</button></div>`
         div_right.appendChild(nw)
     }
 }
@@ -17,6 +17,30 @@ fetch("https://openapi.programming-hero.com/api/plants")
 .then(x => x.json())
 .then(x => middle_card(x.plants))
 let middle_card = (v) =>{
+  let middle_div = document.getElementById("div_middle")
+  middle_div.innerHTML =""
+  for(let card of v){
+    let nw = document.createElement("div")
+    nw.innerHTML =`<div class="flex flex-col bg-white p-3 gap-2.5 h-full">
+        <img class="w-full h-48 object-cover" src="${card.image}" alt="">
+        <h1 class="font-bold">${card.name}</h1>
+        <p>${card.description}</p>
+        <div class="flex justify-between">
+          <button class="px-3 bg-green-500 rounded-2xl">${card.category}</button>
+          <h1>$ <span>${card.price}</span></h1>
+        </div>
+        <button class="bg-green-700 rounded-2xl"> add to card</button>
+      </div>`
+      middle_div.appendChild(nw)
+  }
+}
+/**middle card by catagory name */
+let catagory_name = (id)=>{
+    fetch(`https://openapi.programming-hero.com/api/category/${id}`)
+    .then(x => x.json())
+    .then(x => middle(x.plants))
+}
+let middle = (v) =>{
   let middle_div = document.getElementById("div_middle")
   middle_div.innerHTML =""
   for(let card of v){
